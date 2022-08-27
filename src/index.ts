3822034573fbfe795355f0de8b4d07216031d2a2
Gianluca Costa <gianluca@gianlucacosta.info>
2022-08-27 16:29:22 +0200

    Export FilterLogger and LogLevel via index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { Logger } from "./Logger";
 export { ArrayLogger } from "./ArrayLogger";
 export { CompositeLogger } from "./CompositeLogger";
+export { FilterLogger } from "./FilterLogger";
+export { LogLevel } from "./LogLevel";
